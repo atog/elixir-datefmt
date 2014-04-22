409cf4c9ffa9b1e4df2c133d417c974ff441593c
Koen Van der Auwera <koen@atog.be>
2014-04-22 22:09:25 +0200

    better, as seen on ecto

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DateFmt.Mixfile do
   def project do
     [ app: :"elixir-datefmt",
       version: "0.0.1",
-      elixir: "~> 0.13.0",
+      elixir: "~> 0.12.4 or ~> 0.13.0-dev",
       deps: deps ]
   end
 
